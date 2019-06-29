751bcc6dd623ef266b217cd1c15999c52ee65d05
TenyiChen <35438449+TenyiChen@users.noreply.github.com>
2019-06-29 17:11:41 +0800

    程鋨碼檔案更新

---
+++ src/main/java/com/tom/Hello.java
@@ -0,0 +1,13 @@
+package com.tom;
+
+public class Hello {
+    public static void main(String[] args) {
+//  System.out.println("Hello Word");
+        //new Person().hello();
+
+        Person p = new Person();
+        p.hello();
+    }
+
+}
+
---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,14 @@
+package com.tom
+fun main() {
+ //  println("Hello Kotlin");
+  Human().Hello()
+
+
+}
+
+public class Human {
+    fun Hello(){
+        println("Hello Kotllin")
+    }
+
+}
